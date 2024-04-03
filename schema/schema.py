8153afc074170471024d7b10d45521ba572aa969
HalfMillennium <glc2266@gmail.com>
2024-04-03 04:10:14 -0400

    Wrap fetch_diary_entry and fetch_entity_images functions in a MnemoService class. Introduce datetime tools and randomly generated times into schema.py. Implement within CreateEntityImages and CreateDiaryEntry mutations.

--- schema/schema.py
+++ schema/schema.py
@@ -1,23 +1,28 @@
 import graphene
 from graphene import relay
+import random
+from datetime import date
 from graphql_relay import from_global_id
 from .models import Image, ImageType, DiaryEntry, DiaryEntryType
-from ..core.service import MnemoService
+from ..core.service.server import MnemoService
 
 class CreateDiaryEntryMutation(relay.ClientIDMutation):
     class Input:
         entity_name = graphene.String(required=True)
-        date = graphene.String(requried=True)
+        date = graphene.String(required=True)
         id = graphene.ID()
 
     # Class attributes define the response of the mutation
     diary_entry = graphene.Field(DiaryEntryType)
 
     @classmethod
-    def mutate_and_get_payload(cls, root, info, entity_name, date, id):
-
-        diary_entry = DiaryEntry.objects.get(entity_name, date)
-
+    def mutate_and_get_payload(cls, root, info, entity_name):
+        current_date = date.today().strftime('%Y-%m-%d')
+        diary_entry = None
+        try:
+            diary_entry = DiaryEntry.objects.get(entity_name=entity_name, date=current_date)
+        except DiaryEntry.DoesNotExist:
+            pass
         if(not diary_entry):
             '''
             Otherwise generate new entry
@@ -25,28 +30,33 @@ class CreateDiaryEntryMutation(relay.ClientIDMutation):
             '''
             mnemo_service = MnemoService()
             content = mnemo_service.fetch_diary_entry(entity_name)
-            return CreateDiaryEntryMutation(diary_entry=content)
+            random_time_of_day = f'{random.randint(0, 23)}:{random.randint(0, 59)} EST'
 
-        return CreateDiaryEntryMutation(diary_entry)
+            diary_entry = DiaryEntry.objects.create(entity_name=entity_name, date=current_date, time=random_time_of_day, content=content)
+            return CreateDiaryEntryMutation(diary_entry=diary_entry)
+
+        return CreateDiaryEntryMutation(diary_entry=diary_entry)
 
 class CreateEntityImageSetMutation(relay.ClientIDMutation):
     class Input:
         entity_name = graphene.String(required=True)
         date = graphene.String(required=True)
-        id = graphene.ID()
 
     images = graphene.List(ImageType)
 
     @classmethod
     def mutate_and_get_payload(cls, root, info, entity_name):
+        current_date = date.today().strftime('%m-%d')
+        image_set = Image.objects.filter(entity_name=entity_name, date_month=current_date)
+        if(image_set):
+            return CreateEntityImageSetMutation(images=image_set)
+        
         mnemo_service = MnemoService()
-        images_list = mnemo_service.fetch_images(entity_name)
-
+        images_list = mnemo_service.fetch_entity_images(entity_name)
         image_objects = []
         for img_data in images_list:
-            image = Image.objects.create(src=img_data["src"], alt=img_data["alt"])
+            image = Image.objects.create(entity_name, date_month=current_date, src=img_data["src"], alt=img_data["alt"])
             image_objects.append(image)
-
         return CreateEntityImageSetMutation(images=image_objects)
 
 class Mutation(graphene.ObjectType):
